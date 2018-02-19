19b3b335396057e24dd6bcb59f58416f148320e0
luismiguelcruz <luismiguelcruz@gmail.com>
2018-02-19 13:28:42 +0000

    renaming constants

--- src/main/java/com/ixxus/activeMqCommunication/bootApp/MessageGeneratorService.java
+++ src/main/java/com/ixxus/activeMqCommunication/bootApp/MessageGeneratorService.java
@@ -3,21 +3,24 @@ package com.ixxus.activeMqCommunication.bootApp;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Service;
 
-public class SpringIntegrationJms {
+@Service
+public class MessageGeneratorService {
     private JmsTemplate jmsTemplate;
 
     public static final String HELLO_WORLD = "Hello World!";
-    public static final String QUEUE_HELLO_WORLD_SPRING_INTEGRATION = "queue_spring_integration_1";
+    public static final String QUEUE_HELLO_WORLD = "queue_spring_integration_1";
+    public static final String BROKER_URL = "tcp://localhost:61616";
 
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        SpringIntegrationJms springIntExample = (SpringIntegrationJms) context.getBean("springIntegrationJms");
-        springIntExample.sendMessage(HELLO_WORLD);
+        MessageGeneratorService messageService = (MessageGeneratorService) context.getBean("messageGeneratorService");
+        messageService.sendMessage(HELLO_WORLD);
     }
 
     public void sendMessage(String message) {
-        getJmsTemplate().convertAndSend(QUEUE_HELLO_WORLD_SPRING_INTEGRATION, message);
+        getJmsTemplate().convertAndSend(QUEUE_HELLO_WORLD, message);
     }
 
     public JmsTemplate getJmsTemplate() {

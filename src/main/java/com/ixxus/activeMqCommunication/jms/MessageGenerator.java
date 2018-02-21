2367d1f751d56ebe67b499c6b558ff55a8154f27
luismiguelcruz <luismiguelcruz@gmail.com>
2018-02-21 14:08:53 +0000

    Refactoring

--- src/main/java/com/ixxus/activeMqCommunication/jms/MessageGenerator.java
+++ src/main/java/com/ixxus/activeMqCommunication/jms/MessageGenerator.java
@@ -1,7 +1,5 @@
 package com.ixxus.activeMqCommunication.jms;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
 
